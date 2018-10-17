6ad5754856a4a3287efe14607267d43e9ae99230
Adam Wołonkiewicz <yamatano18@gmail.com>
2018-10-17 18:59:41 +0200

    camel case for table names, new table Deliveries

--- wutee_recs.sql
+++ wutee_recs.sql
@@ -1,63 +1,80 @@
 -- phpMyAdmin SQL Dump
--- version 4.7.4
--- https://www.phpmyadmin.net/
+-- version home.pl
+-- http://www.phpmyadmin.net
 --
--- Host: 127.0.0.1
--- Czas generowania: 16 Paź 2018, 18:28
--- Wersja serwera: 10.1.26-MariaDB
--- Wersja PHP: 7.1.9
+-- Host: localhost
+-- Czas wygenerowania: 17 Paź 2018, 18:58
+-- Wersja serwera: 5.7.22-22-log
+-- Wersja PHP: 7.1.15
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
-START TRANSACTION;
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+/*!40101 SET NAMES utf8 */;
 
 --
--- Baza danych: `wutee_recs`
+-- Baza danych: `07500260_propsy`
 --
 
 -- --------------------------------------------------------
 
 --
--- Struktura tabeli dla tabeli `deliverymans`
+-- Struktura tabeli dla tabeli `deliveries`
 --
 
-CREATE TABLE `deliverymans` (
-  `deliveryManId` int(8) NOT NULL,
+CREATE TABLE IF NOT EXISTS `deliveries` (
+  `deliveryId` int(8) NOT NULL AUTO_INCREMENT,
+  `userId` int(8) NOT NULL,
+  `orderId` int(8) NOT NULL,
+  PRIMARY KEY (`deliveryId`),
+  KEY `userId` (`userId`,`orderId`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin2 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `deliveryMans`
+--
+
+CREATE TABLE IF NOT EXISTS `deliveryMans` (
+  `deliveryManId` int(8) NOT NULL AUTO_INCREMENT,
   `name` varchar(50) COLLATE utf8_polish_ci NOT NULL,
   `surname` varchar(50) COLLATE utf8_polish_ci NOT NULL,
-  `orderId` int(11) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci;
+  PRIMARY KEY (`deliveryManId`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
 --
--- Struktura tabeli dla tabeli `foodinmenus`
+-- Struktura tabeli dla tabeli `foodInMenus`
 --
 
-CREATE TABLE `foodinmenus` (
+CREATE TABLE IF NOT EXISTS `foodInMenus` (
   `menuId` int(8) NOT NULL,
-  `foodId` int(8) NOT NULL,
-  `price` float NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci;
+  `foodId` int(8) NOT NULL AUTO_INCREMENT,
+  `price` float NOT NULL,
+  PRIMARY KEY (`foodId`),
+  KEY `menuId` (`menuId`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
 --
--- Struktura tabeli dla tabeli `foodinorders`
+-- Struktura tabeli dla tabeli `foodInOrders`
 --
 
-CREATE TABLE `foodinorders` (
-  `foodInOrderId` int(8) NOT NULL,
+CREATE TABLE IF NOT EXISTS `foodInOrders` (
+  `foodInOrderId` int(8) NOT NULL AUTO_INCREMENT,
   `orderId` int(8) NOT NULL,
-  `foodId` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci;
+  `foodId` int(11) NOT NULL,
+  PRIMARY KEY (`foodInOrderId`),
+  KEY `orderId` (`orderId`),
+  KEY `foodId` (`foodId`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
@@ -65,16 +82,17 @@ CREATE TABLE `foodinorders` (
 -- Struktura tabeli dla tabeli `foods`
 --
 
-CREATE TABLE `foods` (
-  `foodId` int(8) NOT NULL,
+CREATE TABLE IF NOT EXISTS `foods` (
+  `foodId` int(8) NOT NULL AUTO_INCREMENT,
   `foodName` varchar(50) COLLATE utf8_polish_ci NOT NULL,
   `foodDescription` varchar(200) COLLATE utf8_polish_ci DEFAULT NULL,
   `calories` float DEFAULT NULL,
-  `isSpicy` int(8) DEFAULT NULL,
-  `isVege` int(8) DEFAULT NULL,
-  `isGlutenFree` int(8) DEFAULT NULL,
-  `photoLocation` varchar(200) COLLATE utf8_polish_ci DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci;
+  `isSpicy` tinyint(1) DEFAULT NULL,
+  `isVege` tinyint(1) DEFAULT NULL,
+  `isGlutenFree` tinyint(1) DEFAULT NULL,
+  `photoLocation` varchar(200) COLLATE utf8_polish_ci DEFAULT NULL,
+  PRIMARY KEY (`foodId`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
@@ -82,36 +100,40 @@ CREATE TABLE `foods` (
 -- Struktura tabeli dla tabeli `ingredients`
 --
 
-CREATE TABLE `ingredients` (
-  `ingredientId` int(8) NOT NULL,
+CREATE TABLE IF NOT EXISTS `ingredients` (
+  `ingredientId` int(8) NOT NULL AUTO_INCREMENT,
   `ingredientName` varchar(50) COLLATE utf8_polish_ci NOT NULL,
   `photoLocation` varchar(100) COLLATE utf8_polish_ci DEFAULT NULL,
-  `ingredientDescription` varchar(200) COLLATE utf8_polish_ci DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci;
+  `ingredientDescription` varchar(200) COLLATE utf8_polish_ci DEFAULT NULL,
+  PRIMARY KEY (`ingredientId`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
 --
--- Struktura tabeli dla tabeli `ingredientsiningredientsorder`
+-- Struktura tabeli dla tabeli `ingredientsOrders`
 --
 
-CREATE TABLE `ingredientsiningredientsorder` (
-  `ingredientOrderId` int(8) NOT NULL,
-  `ingredientId` int(8) NOT NULL,
-  `quantity` int(8) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci;
-
--- --------------------------------------------------------
-
---
--- Struktura tabeli dla tabeli `ingredientsorders`
---
-
-CREATE TABLE `ingredientsorders` (
-  `ingredientsOrderId` int(8) NOT NULL,
+CREATE TABLE IF NOT EXISTS `ingredientsOrders` (
+  `ingredientsOrderId` int(8) NOT NULL AUTO_INCREMENT,
   `date` date DEFAULT NULL,
-  `comment` varchar(400) COLLATE utf8_polish_ci DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci;
+  `comment` varchar(400) COLLATE utf8_polish_ci DEFAULT NULL,
+  PRIMARY KEY (`ingredientsOrderId`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `ingredientsToOrder`
+--
+
+CREATE TABLE IF NOT EXISTS `ingredientsToOrder` (
+  `ingredientOrderId` int(8) NOT NULL AUTO_INCREMENT,
+  `ingredientId` int(8) NOT NULL,
+  `quantity` int(8) NOT NULL,
+  PRIMARY KEY (`ingredientOrderId`),
+  KEY `ingredientId` (`ingredientId`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
@@ -119,10 +141,11 @@ CREATE TABLE `ingredientsorders` (
 -- Struktura tabeli dla tabeli `languages`
 --
 
-CREATE TABLE `languages` (
-  `languageId` int(8) NOT NULL,
-  `phrase` varchar(200) COLLATE utf8_polish_ci NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci;
+CREATE TABLE IF NOT EXISTS `languages` (
+  `languageId` int(8) NOT NULL AUTO_INCREMENT,
+  `phrase` varchar(200) COLLATE utf8_polish_ci NOT NULL,
+  PRIMARY KEY (`languageId`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
@@ -130,10 +153,11 @@ CREATE TABLE `languages` (
 -- Struktura tabeli dla tabeli `menus`
 --
 
-CREATE TABLE `menus` (
-  `menuId` int(8) NOT NULL,
-  `menuName` varchar(100) COLLATE utf8_polish_ci NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci;
+CREATE TABLE IF NOT EXISTS `menus` (
+  `menuId` int(8) NOT NULL AUTO_INCREMENT,
+  `menuName` varchar(100) COLLATE utf8_polish_ci NOT NULL,
+  PRIMARY KEY (`menuId`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
@@ -141,8 +165,8 @@ CREATE TABLE `menus` (
 -- Struktura tabeli dla tabeli `orders`
 --
 
-CREATE TABLE `orders` (
-  `orderId` int(8) NOT NULL,
+CREATE TABLE IF NOT EXISTS `orders` (
+  `orderId` int(8) NOT NULL AUTO_INCREMENT,
   `userId` int(8) NOT NULL,
   `deliveryManId` int(8) NOT NULL,
   `restauranId` int(8) NOT NULL,
@@ -153,19 +177,24 @@ CREATE TABLE `orders` (
   `userOpinion` varchar(400) COLLATE utf8_polish_ci DEFAULT NULL,
   `userComment` varchar(400) COLLATE utf8_polish_ci DEFAULT NULL,
   `deliveryManComment` varchar(400) COLLATE utf8_polish_ci DEFAULT NULL,
-  `loyaltyPoints` int(8) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci;
+  `loyaltyPoints` int(8) DEFAULT NULL,
+  PRIMARY KEY (`orderId`),
+  KEY `userId` (`userId`),
+  KEY `deliveryManId` (`deliveryManId`),
+  KEY `restauranId` (`restauranId`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
 --
--- Struktura tabeli dla tabeli `paymentmethods`
+-- Struktura tabeli dla tabeli `paymentMethods`
 --
 
-CREATE TABLE `paymentmethods` (
-  `methodId` int(8) NOT NULL,
-  `paymentName` varchar(50) COLLATE utf8_polish_ci NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci;
+CREATE TABLE IF NOT EXISTS `paymentMethods` (
+  `methodId` int(8) NOT NULL AUTO_INCREMENT,
+  `paymentName` varchar(50) COLLATE utf8_polish_ci NOT NULL,
+  PRIMARY KEY (`methodId`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
@@ -173,11 +202,13 @@ CREATE TABLE `paymentmethods` (
 -- Struktura tabeli dla tabeli `payments`
 --
 
-CREATE TABLE `payments` (
-  `paymentId` int(8) NOT NULL,
+CREATE TABLE IF NOT EXISTS `payments` (
+  `paymentId` int(8) NOT NULL AUTO_INCREMENT,
   `methodId` int(8) NOT NULL,
-  `status` int(8) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci;
+  `status` int(8) NOT NULL,
+  PRIMARY KEY (`paymentId`),
+  KEY `methodId` (`methodId`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
@@ -185,26 +216,30 @@ CREATE TABLE `payments` (
 -- Struktura tabeli dla tabeli `restaurants`
 --
 
-CREATE TABLE `restaurants` (
-  `restaurantId` int(8) NOT NULL,
-  `nameSlug` varchar(50) COLLATE utf8_polish_ci DEFAULT NULL,
+CREATE TABLE IF NOT EXISTS `restaurants` (
+  `restaurantId` int(8) NOT NULL AUTO_INCREMENT,
+  `restaurantName` varchar(50) COLLATE utf8_polish_ci NOT NULL,
   `adress` varchar(50) COLLATE utf8_polish_ci DEFAULT NULL,
-  `ownerId` int(8) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci;
+  `ownerId` int(8) NOT NULL,
+  PRIMARY KEY (`restaurantId`),
+  KEY `ownerId` (`ownerId`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
 --
--- Struktura tabeli dla tabeli `restaurantworkers`
+-- Struktura tabeli dla tabeli `restaurantWorkers`
 --
 
-CREATE TABLE `restaurantworkers` (
-  `workerId` int(8) NOT NULL,
+CREATE TABLE IF NOT EXISTS `restaurantWorkers` (
+  `workerId` int(8) NOT NULL AUTO_INCREMENT,
   `workerName` varchar(50) COLLATE utf8_polish_ci NOT NULL,
   `workerSurname` varchar(50) COLLATE utf8_polish_ci NOT NULL,
   `workingStatus` varchar(20) COLLATE utf8_polish_ci NOT NULL DEFAULT ' unemployed',
-  `restaurantId` int(8) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci;
+  `restaurantId` int(8) NOT NULL,
+  PRIMARY KEY (`workerId`),
+  KEY `restaurantId` (`restaurantId`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
@@ -212,10 +247,12 @@ CREATE TABLE `restaurantworkers` (
 -- Struktura tabeli dla tabeli `translations`
 --
 
-CREATE TABLE `translations` (
+CREATE TABLE IF NOT EXISTS `translations` (
   `slug` varchar(100) COLLATE utf8_polish_ci NOT NULL,
   `languageId` int(8) NOT NULL,
-  `translation` varchar(200) COLLATE utf8_polish_ci NOT NULL
+  `translation` varchar(200) COLLATE utf8_polish_ci NOT NULL,
+  PRIMARY KEY (`slug`),
+  KEY `languageId` (`languageId`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci;
 
 -- --------------------------------------------------------
@@ -224,8 +261,8 @@ CREATE TABLE `translations` (
 -- Struktura tabeli dla tabeli `users`
 --
 
-CREATE TABLE `users` (
-  `userId` int(8) NOT NULL,
+CREATE TABLE IF NOT EXISTS `users` (
+  `userId` int(8) NOT NULL AUTO_INCREMENT,
   `name` varchar(50) COLLATE utf8_polish_ci NOT NULL,
   `mail` varchar(50) COLLATE utf8_polish_ci NOT NULL,
   `role` int(8) NOT NULL,
@@ -233,214 +270,9 @@ CREATE TABLE `users` (
   `adress` varchar(50) COLLATE utf8_polish_ci DEFAULT NULL,
   `city` varchar(30) COLLATE utf8_polish_ci DEFAULT NULL,
   `phone` varchar(20) COLLATE utf8_polish_ci DEFAULT NULL,
-  `loyaltyPoints` int(8) NOT NULL DEFAULT '0'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci;
-
---
--- Indeksy dla zrzutów tabel
---
-
---
--- Indexes for table `deliverymans`
---
-ALTER TABLE `deliverymans`
-  ADD PRIMARY KEY (`deliveryManId`),
-  ADD KEY `orderId` (`orderId`);
-
---
--- Indexes for table `foodinmenus`
---
-ALTER TABLE `foodinmenus`
-  ADD PRIMARY KEY (`foodId`),
-  ADD KEY `menuId` (`menuId`);
-
---
--- Indexes for table `foodinorders`
---
-ALTER TABLE `foodinorders`
-  ADD PRIMARY KEY (`foodInOrderId`),
-  ADD KEY `orderId` (`orderId`),
-  ADD KEY `foodId` (`foodId`);
-
---
--- Indexes for table `foods`
---
-ALTER TABLE `foods`
-  ADD PRIMARY KEY (`foodId`);
-
---
--- Indexes for table `ingredients`
---
-ALTER TABLE `ingredients`
-  ADD PRIMARY KEY (`ingredientId`);
-
---
--- Indexes for table `ingredientsiningredientsorder`
---
-ALTER TABLE `ingredientsiningredientsorder`
-  ADD PRIMARY KEY (`ingredientOrderId`),
-  ADD KEY `ingredientId` (`ingredientId`);
-
---
--- Indexes for table `ingredientsorders`
---
-ALTER TABLE `ingredientsorders`
-  ADD PRIMARY KEY (`ingredientsOrderId`);
-
---
--- Indexes for table `languages`
---
-ALTER TABLE `languages`
-  ADD PRIMARY KEY (`languageId`);
-
---
--- Indexes for table `menus`
---
-ALTER TABLE `menus`
-  ADD PRIMARY KEY (`menuId`);
-
---
--- Indexes for table `orders`
---
-ALTER TABLE `orders`
-  ADD PRIMARY KEY (`orderId`),
-  ADD KEY `userId` (`userId`),
-  ADD KEY `deliveryManId` (`deliveryManId`),
-  ADD KEY `restauranId` (`restauranId`);
-
---
--- Indexes for table `paymentmethods`
---
-ALTER TABLE `paymentmethods`
-  ADD PRIMARY KEY (`methodId`);
-
---
--- Indexes for table `payments`
---
-ALTER TABLE `payments`
-  ADD PRIMARY KEY (`paymentId`),
-  ADD KEY `methodId` (`methodId`);
-
---
--- Indexes for table `restaurants`
---
-ALTER TABLE `restaurants`
-  ADD PRIMARY KEY (`restaurantId`),
-  ADD KEY `ownerId` (`ownerId`);
-
---
--- Indexes for table `restaurantworkers`
---
-ALTER TABLE `restaurantworkers`
-  ADD PRIMARY KEY (`workerId`),
-  ADD KEY `restaurantId` (`restaurantId`);
-
---
--- Indexes for table `translations`
---
-ALTER TABLE `translations`
-  ADD PRIMARY KEY (`slug`),
-  ADD KEY `languageId` (`languageId`);
-
---
--- Indexes for table `users`
---
-ALTER TABLE `users`
-  ADD PRIMARY KEY (`userId`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT dla tabeli `deliverymans`
---
-ALTER TABLE `deliverymans`
-  MODIFY `deliveryManId` int(8) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT dla tabeli `foodinmenus`
---
-ALTER TABLE `foodinmenus`
-  MODIFY `foodId` int(8) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT dla tabeli `foodinorders`
---
-ALTER TABLE `foodinorders`
-  MODIFY `foodInOrderId` int(8) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT dla tabeli `foods`
---
-ALTER TABLE `foods`
-  MODIFY `foodId` int(8) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT dla tabeli `ingredients`
---
-ALTER TABLE `ingredients`
-  MODIFY `ingredientId` int(8) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT dla tabeli `ingredientsiningredientsorder`
---
-ALTER TABLE `ingredientsiningredientsorder`
-  MODIFY `ingredientOrderId` int(8) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT dla tabeli `ingredientsorders`
---
-ALTER TABLE `ingredientsorders`
-  MODIFY `ingredientsOrderId` int(8) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT dla tabeli `languages`
---
-ALTER TABLE `languages`
-  MODIFY `languageId` int(8) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT dla tabeli `menus`
---
-ALTER TABLE `menus`
-  MODIFY `menuId` int(8) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT dla tabeli `orders`
---
-ALTER TABLE `orders`
-  MODIFY `orderId` int(8) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT dla tabeli `paymentmethods`
---
-ALTER TABLE `paymentmethods`
-  MODIFY `methodId` int(8) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT dla tabeli `payments`
---
-ALTER TABLE `payments`
-  MODIFY `paymentId` int(8) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT dla tabeli `restaurants`
---
-ALTER TABLE `restaurants`
-  MODIFY `restaurantId` int(8) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT dla tabeli `restaurantworkers`
---
-ALTER TABLE `restaurantworkers`
-  MODIFY `workerId` int(8) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT dla tabeli `users`
---
-ALTER TABLE `users`
-  MODIFY `userId` int(8) NOT NULL AUTO_INCREMENT;
+  `loyaltyPoints` int(8) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`userId`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci AUTO_INCREMENT=1 ;
 
 --
 -- Ograniczenia dla zrzutów tabel
@@ -451,7 +283,6 @@ ALTER TABLE `users`
 --
 ALTER TABLE `orders`
   ADD CONSTRAINT `orders_ibfk_1` FOREIGN KEY (`userId`) REFERENCES `users` (`userId`);
-COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
